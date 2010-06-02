b606f8086c6d0562369261facfd20cb3fe590dac
Scott Crosby <scrosby@cs.rice.edu>
2010-06-02 14:49:50 -0500

    Caching signer framework.

---
+++ org/rice/crosby/batchsig/CachingSigner.java
@@ -0,0 +1,22 @@
+package org.rice.crosby.batchsig;
+
+/** Wraps a signer with one that caches signature verifications so that the same signature and data need only be verified once */
+public class CachingSigner implements Signer {
+	final private Signer orig;
+
+	CachingSigner(Signer orig) {
+		this.orig = orig;
+	}
+		
+	@Override
+	public byte[] sign(byte[] data) {
+		return orig.sign(data);
+	}
+
+	@Override
+	public boolean verify(byte[] data, byte[] sig) {
+		// TODO Implement the cache.
+		return orig.verify(data,sig);
+	}
+
+}
