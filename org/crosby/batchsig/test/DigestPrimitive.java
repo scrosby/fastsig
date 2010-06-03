8e942dce60155e752f02d31bf0d082220183ad8f
Scott Crosby <scrosby@cs.rice.edu>
2010-06-03 12:55:15 -0500

    New testing code.

--- org/crosby/batchsig/test/DigestPrimitive.java
+++ org/crosby/batchsig/test/DigestPrimitive.java
@@ -5,11 +5,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import org.rice.crosby.batchsig.SignaturePrimitives;
+import org.rice.crosby.historytree.generated.Serialization.SigTreeType;
 import org.rice.crosby.historytree.generated.Serialization.TreeSigBlob;
+import org.rice.crosby.historytree.generated.Serialization.TreeSigMessage;
 import org.rice.crosby.historytree.generated.Serialization.TreeSigBlob.Builder;
 import org.rice.crosby.historytree.generated.Serialization.TreeSigBlob.SignatureAlgorithm;
 
 import com.google.protobuf.ByteString;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 
 /** 'Sign' a string by computing a digest over it. 
@@ -26,7 +29,7 @@ public class DigestPrimitive implements SignaturePrimitives {
 		try {
 			MessageDigest md=MessageDigest.getInstance("SHA-256");
 			md.update(data);
-			return md.digest();
+			return Base64.encode(md.digest()).getBytes();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 			return null;
@@ -34,13 +37,17 @@ public class DigestPrimitive implements SignaturePrimitives {
 	}
 	
 	@Override
-	public void sign(byte[] data, Builder out) {
+	public void sign(byte[] data, TreeSigBlob.Builder out) {
 		out.setSignatureAlgorithm(SignatureAlgorithm.TEST_DIGEST);
+
+		//System.out.println("Signing '"+new String(data)+"' with "+new String(hash(data)));
+		
 		out.setSignatureBytes(ByteString.copyFrom(hash(data)));
 	}
 
 	@Override
 	public boolean verify(byte[] data, TreeSigBlob sig) {
+		//System.out.println("Validating '"+new String(data)+"' : "+new String(hash(data)) + "  == " + new String(sig.getSignatureBytes().toByteArray()));
 		if (sig.getSignatureAlgorithm() == SignatureAlgorithm.TEST_DIGEST)
 			return Arrays.equals(hash(data),sig.getSignatureBytes().toByteArray());
 		System.out.println("A non-'test' signature sent under test");
--- org/crosby/batchsig/test/TestSimpleQueue.java
+++ org/crosby/batchsig/test/TestSimpleQueue.java
@@ -1,8 +1,89 @@
 package org.crosby.batchsig.test;
 
+import org.junit.Test;
+import org.rice.crosby.batchsig.Message;
+import org.rice.crosby.batchsig.SimpleQueue;
+import org.rice.crosby.batchsig.VerifyQueue;
+import org.rice.crosby.historytree.generated.Serialization.TreeSigBlob;
+
 import junit.framework.TestCase;
 
 
 public class TestSimpleQueue extends TestCase {
+	class MessageWrap implements Message {
+		byte data[];
+		TreeSigBlob signature;
+		Boolean targetvalidity = null;
+		
+		public MessageWrap(int i) {
+			data = String.format("Foo%d",i).getBytes(); 
+		}
 
+		@Override
+		public byte[] getData() {
+			return data;
+		}
+
+		@Override
+		public Object getRecipient() {
+			return new Object();
+		}
+
+		@Override
+		public TreeSigBlob getSignatureBlob() {
+			return this.signature;
+		}
+
+		@Override
+		public Object getSigner() {
+			return getClass();
+		}
+
+		@Override
+		public void signatureResult(TreeSigBlob sig) {
+			//System.out.format("Signed '%s' with sig: {{%s}}\n" , new String(data) ,sig.toString());
+			this.signature = sig;
+		}
+
+		@Override
+		public void signatureValidity(boolean valid) {
+			assertEquals(targetvalidity.booleanValue(),valid);
+			targetvalidity = null;
+		}
+
+		public void wantValid() {targetvalidity = true;}
+		public void wantInValid() {targetvalidity = false;}
+	}
+	
+	
+	@Test 
+	public void testInsertAndProcess() {
+		SimpleQueue signqueue = new SimpleQueue(new DigestPrimitive());
+		
+		MessageWrap msg1 = new MessageWrap(1001);
+		MessageWrap msg2 = new MessageWrap(1002);
+		MessageWrap msg3 = new MessageWrap(1003);
+
+		signqueue.add(msg1);
+		signqueue.add(msg2);
+		signqueue.add(msg3);
+		signqueue.process();
+
+		// Corrupt message 2.
+		msg2.data[1]=0;
+		
+		msg1.wantValid();
+		msg2.wantInValid();
+		msg3.wantValid();
+
+		VerifyQueue verifyqueue = new VerifyQueue(new DigestPrimitive());
+		verifyqueue.add(msg1);
+		verifyqueue.add(msg2);
+		verifyqueue.add(msg3);
+		verifyqueue.process();
+
+		assertNull(msg1.targetvalidity);
+		assertNull(msg2.targetvalidity);
+		assertNull(msg3.targetvalidity);
+	}
 }
