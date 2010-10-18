7f52c6486c5398fc24c1b53562d0550764fbbc72
Scott Crosby <scrosby@cs.rice.edu>
2010-10-18 00:33:31 -0500

    Revamp onetree to account for actual dependency graph, with virtual nodes for bundle-ends.

--- src/edu/rice/batchsig/splice/OneTree.java
+++ src/edu/rice/batchsig/splice/OneTree.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 
@@ -27,36 +28,39 @@ public class OneTree {
 	}
 	
 	final private VerifyHisttreeLazily verifier;
-	/** For each version number the message at that number. */
-	HashMap<Integer,IncomingMessage> messages = new LinkedHashMap<Integer,IncomingMessage>(1,.75f,false);
-
-	/** Cache of unvalidated root hashes of the history trees for messages in messages. msg.getTree().agg()
-	 *    When we verify a splice, we need to take the child message's agg() and compare it to aggV(child.version).
-	 *    Rather than rebuild the child's pruned tree, or cache the whole thing, just cache the parts we need, the agg().
+	
+	/** Map from an integer version number to the message at that version number */
+	HashMap<Integer,IncomingMessage> bundles = new LinkedHashMap<Integer,IncomingMessage>(1,.75f,false);
+	
+	/** Cache of root hashes for each unvalidated bundle.
+	 *    When we verify a splice, we need to take the predecessor's bundle agg() and compare it to aggV(pred.version).
+	 *    Rather than rebuild the pred's pruned tree, or cache the whole thing, just cache the parts we need, the agg().
 	 *  */
-	HashMap<Integer,byte[]> roothashes = new HashMap<Integer,byte[]>();
-
-	/** Each node in the Dag corresponds the end-of-an-epoch bundle. This maps the version number that denotes a bundle to the set of messgaes in that bundle */
-	Multimap<Integer,IncomingMessage> buckets = LinkedListMultimap.create();
-
-	/** For various message indices, cache of the validated root hashes of history trees; if a new message arrives with a hash in this list, then the message is validated without a public key op. */
-	HashMap<Integer,byte[]> validatedhashes = new HashMap<Integer,byte[]>();
+	HashMap<Integer,ByteString> roothashes = new HashMap<Integer,ByteString>();
+	
+	/** This hashmap finds the message signatures we need to verify to validate a given bundle.
+	 * 
+	 * When we find the 'root' node of a dependency tree, that will be an exlempar, but not correspond to a 'real' message. This will.
+	 * 
+	 */
+	HashMap<Integer,IncomingMessage> validators = new HashMap<Integer,IncomingMessage>();
 	
 	/**
 	 * Invariant; The dag contains nodes for each message and the version
-	 * numbers of the splicepoints for each message.
+	 * numbers of the splicepoints seen for that bundle.
 	 * 
-	 * If an edge is in the dag and both messages have been seen (are in the
-	 * messages hash), then the splice has been VERIFIED.
+	 * If an edge is in the dag and both bundles have been seen (are in the
+	 * bundles hash), then the splice has been VERIFIED.
 	 * 
-	 * If an edge is in the dag, but either the from or to messages in the
-	 * dag are *NOT* in the messages hash, then the splice is provisional
+	 * If an edge is in the dag, but either the from or to bundles in the
+	 * dag are *NOT* in the bundles hash, then the splice is provisional
 	 * and may not exist. We wait until the peer message arrives, verify the
 	 * splice, and keep the edge if the splice verifies, or remove the edge
 	 * if the splice fails.
 	 * 
 	 * */
 	final private Dag<Integer> dag = new Dag<Integer>();
+
 	final private Object author;
 	final private long treeid;
 
@@ -84,77 +88,101 @@ public class OneTree {
 		this.treeid = l;
 		this.verifier = verifier;
 	}
+
+	private void failMessage(IncomingMessage m) {
+		m.signatureValidity(true);
+		size--;
+	}
+	
+	/*
+	 *  Errors: See this messages bundle before with a different hash. Verify immediately.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 	
 	
-	/* Each node in the dag corresponds to a set of messages. All that end in the same epoch. */
+	
+	
+	/* Each node in the dag corresponds to a set of bundles. All that end in the same epoch. */
 	void addMessage(IncomingMessage m) {
 		System.out.println("Adding message "+m);
 		size++;
 		Integer key = m.getSignatureBlob().getLeaf();
 		Integer bundlekey = m.getSignatureBlob().getTree().getVersion();
 		HistoryTree<byte[],byte[]> tree = VerifyHisttreeLazily.parseHistoryTree(m);
-
-		// First, see if this message is well-formed.
+		ByteString agg = ByteString.copyFrom(tree.agg());
+		
+		// First, see if this message is well-formed in the bundle?   Yes. It is.
 		if (!Verifier.checkLeaf(m,tree)) {
 			System.out.println("Broken proof that doesn't validate message in proof.");
-		}
-		
-		// First: Check to see if this message is in the validated hashes.
-		if (validatedhashes.get(key) != null && Arrays.equals(tree.agg(),validatedhashes.get(key))) {
-			// Already done!
-			System.out.println("Hash has been seen and validated already!");
-			m.signatureValidity(true);
+			failMessage(m);
 			return;
 		}
 		
-		// Nope. Now enter the core processing loop.
-		
-		if (messages.containsKey(key)) {
-			// PROBLEM: Have a message with this version number already.
-			// TODO: Can't handle these at all. Only solution: Verify it immediately.
-			throw new Error("TODO");
+		// First, have we seen this ending bundle before?
+		if (validators.containsKey(bundlekey)) {
+			// We got a bundle already. Does it have the same agg?
+			if (!roothashes.get(bundlekey).equals(agg)) {
+				// PROBLEM: Have an inconsistent bundle already.
+				// TODO: Can't handle these at all. Only solution: Verify it immediately.
+				throw new Error("TODO");
+			}
+			System.out.println("Adding to existing bundle");
 		}
-
-		// By definition, at this point, we have never seen this message before.
-		Dag<Integer>.DagNode node = getNode(m);
+		validators.put(bundlekey,m);
 		
-		// PART 1: See if we've seen later messages we might splice into.
-		// This case should be rare and only occur when messages arrive out-of-order.
-		for (Dag<Integer>.DagNode succ : node.getParents()) {
+		// At this point, we know that any keys in this bundle all have the same ending hash, ergo,
+		// the same contents. We don't have to worry about inconsistency anymore, and can just store the data, except for validating splices.
+		
+		// Now, build an edge in the dag from the integer representing the bundle to this message.
+		Dag<Integer>.DagNode node = dag.makeOrGet(key);
+		Dag<Integer>.DagNode bundlenode = dag.makeOrGet(bundlekey);
+
+		// Add a dependency edge for the bundle.
+		if (!node.get().equals(bundlenode.get()))
+			dag.addEdge(bundlenode, node);
+		// And put the message in.
+		bundles.put(key,m);
+				
+		// PART 1: See if we've seen later bundles we might splice into.
+		// This case should be rare and only occur when bundles arrive out-of-order.
+		for (Dag<Integer>.DagNode succ : bundlenode.getParents()) {
 			// For each later message in the dag that provisionally splices this message.
+			System.out.println("Looking at later bundles");
 			Integer succi = succ.get();
-			Message succm = messages.get(succi);
+			Message succm = bundles.get(succi);
 			if (succm == null)
 				throw new Error("Algorithm bug.");
 			// Time to verify the splice is OK. 
 			HistoryTree<byte[],byte[]> succtree = VerifyHisttreeLazily.parseHistoryTree(succm);
-			if (Arrays.equals(succtree.aggV(key.intValue()),tree.agg())) {
+			if (Arrays.equals(succtree.aggV(bundlekey.intValue()),tree.agg())) {
 				System.out.println("Unusual splice circumstance -- success");
-				; // Splice verifies. Nothing to be done.
+				dag.addEdge(succ,bundlenode);
 			} else {
 				// Splice fails. Remove the edge.
 				System.out.println("Unusual splice circumstance -- failure & removal");
-				dag.removeEdge(succ,node);
 			}
 		}
 			
-		// PART 2: See which prior messages we splice into.
+		// PART 2: See which prior bundles we splice into.
 		for (Integer predi : m.getSignatureBlob().getSpliceHintList()) {
 			System.out.println("Handling splicehint: "+predi);
-			// For each splicepoint to prior messages in this message,
-			Message predm = messages.get(predi);
-			Dag<Integer>.DagNode prednode;
+			// For each splicepoint to prior bundles in this message,
+			Message predm = bundles.get(predi);
+			Dag<Integer>.DagNode prednode = dag.makeOrGet(predi);
 			// Have we seen the prior message?
 			if (predm == null) {
 				System.out.println("No priors found");
 				// Nope. Add the node to the dag. Add an edge to that child; it'll be provisional
-				prednode = dag.makeOrGet(predi);
 				dag.addEdge(node,prednode);
 			} else {
 				// We have seen that message. We need to check the splice.
-				if (Arrays.equals(tree.aggV(predi.intValue()),roothashes.get(predm))) {
+				if (ByteString.copyFrom(tree.aggV(predi.intValue())).equals(roothashes.get(predm))) {
 					System.out.println("Found a prior. Verified the splice!");
-					; // Splice verifies. Nothing to be done.
+					dag.addEdge(node,prednode);
 				} else {
 					// Splice fails. Remove the edge.
 					System.out.println("Found a prior, but splice failed");
@@ -163,28 +191,32 @@ public class OneTree {
 				}
 			}
 		}
-		//roothashes.put(m,tree.agg());
-		messages.put(key, m);
+		roothashes.put(bundlekey,agg);
+		bundles.put(key, m);
 		System.out.println("Finished handling for message");
 	}
 
 	private void remove(IncomingMessage m) {
-		size--;
 		verifier.removeMessage(m);
-		messages.remove(m.getSignatureBlob().getLeaf());
-		roothashes.remove(m);
-		getNode(m).remove();
+		int index = m.getSignatureBlob().getLeaf();
+		remove(index);
 	}
 
-	
+	private void remove(int index) {
+		if (bundles.remove(index)!= null)
+			size--;
+		validators.remove(index);
+		roothashes.remove(index);
+		}
+
 	void forceOldest() {
 		System.out.format("Forcing oldest message to OneTree\n");
 
-		Iterator<Integer> i = messages.keySet().iterator();
+		Iterator<Integer> i = bundles.keySet().iterator();
 		if (!i.hasNext()) {
 			throw new Error("Can ignore this error, but we shouldn't be forcing the oldest on an empty onetree");
 		}
-		forceMessage(messages.get(i.next()));
+		forceMessage(bundles.get(i.next()));
 	}
 	
 	
@@ -200,10 +232,11 @@ public class OneTree {
 			System.out.println("WhileLoop at rootPath ="+rootPath);
 			Dag<Integer>.DagNode root = rootPath.root();
 			Integer rooti = root.get();
-			IncomingMessage rootm = messages.get(rooti);
+			// An incoming message that nominally validates the root bundle (may be more than one)
+			IncomingMessage rootm = validators.get(rooti);
+			System.out.format("Got root at %d about to see if it verifies %s\n",rooti,rootm);
 			HistoryTree<byte[],byte[]> roottree = verifier.parseHistoryTree(rootm);
 
-			System.out.println("Got root... about to see if it verified");
 			// Verify the root's public key signature.
 			if (verifier.verifyHistoryRoot(rootm,roottree)) {
 				System.out.println("Verified the root's signature - SUCCESS. It is valid");
@@ -213,19 +246,20 @@ public class OneTree {
 				for (Dag<Integer>.DagNode i : descendents) {
 					System.out.println("Traversing descendent to mark as valid:"+i.get());
 					//Integer desci = i.get();
-					IncomingMessage descm = messages.get(i);
+					IncomingMessage descm = bundles.get(i);
 					if (descm != null) {
 						System.out.println("... and marking it as good!");
 						// This message is not provisional. It is valid.
-						descm.signatureValidity(true);
+						if (descm != null)
+							descm.signatureValidity(true);
 						// TODO: Cache the spliced predecessor hashes from this node as being valid?
-						// Remove it from further tracking.
+						// Remove the message from further tracking.
 						remove(descm);
 					}
+					// Remove any vestiges of the node.
+					remove(i.get());
+					i.remove();
 				}
-				// Mark the root as valid.
-				rootm.signatureValidity(true);
-				remove(rootm);
 
 				System.out.format("<<<<<< Done with handling force of %d to %s\n",m.getSignatureBlob().getLeaf(),getName());
 				return;
@@ -244,12 +278,12 @@ public class OneTree {
 	}
 	
 	public void forceAll() {
-		System.out.format("Forcing all messages in OneTree\n");
-		while (!messages.isEmpty())
-			forceMessage(messages.entrySet().iterator().next().getValue());
+		System.out.format("Forcing all bundles in OneTree\n");
+		while (!bundles.isEmpty())
+			forceMessage(bundles.entrySet().iterator().next().getValue());
 	}
 	
 	public boolean isEmpty() {
-		return messages.isEmpty();
+		return bundles.isEmpty();
 	}
 }
