d8326ac1d17738e5092c744f10534f58a4418fce
Scott Crosby <scrosby@cs.rice.edu>
2010-11-29 14:21:10 -0600

    Test code for dag.

---
+++ test/edu/rice/batchsig/TestDag.java
@@ -0,0 +1,34 @@
+package edu.rice.batchsig;
+
+import org.junit.Test;
+
+import edu.rice.batchsig.splice.Dag;
+import edu.rice.batchsig.splice.Dag.DagNode;
+import junit.framework.TestCase;
+
+public class TestDag extends TestCase {
+
+	
+	@Test
+	public void testMakeOne() {
+		Dag<Integer> dag = new Dag<Integer>();
+		Dag<Integer>.DagNode n1 = dag.makeOrGet(1);
+		Dag<Integer>.DagNode n2 = dag.makeOrGet(2);
+		Dag<Integer>.DagNode n3 = dag.makeOrGet(3);
+		Dag<Integer>.DagNode n4 = dag.makeOrGet(4);
+
+		dag.addEdge(n1,n2);
+		TestCase.assertTrue(n1.getChildren().contains(n2));
+		TestCase.assertTrue(n2.getParents().contains(n1));
+
+		dag.addEdge(n2,n3);
+		TestCase.assertTrue(n2.getChildren().contains(n3));
+		TestCase.assertTrue(n3.getParents().contains(n2));
+
+		
+		
+		
+	}
+	
+	
+}
